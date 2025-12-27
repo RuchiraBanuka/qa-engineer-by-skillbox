db61530b754b9f2335e18b741cba4306ec5d84e3
Sergey Matylionak <79189126+matowdev@users.noreply.github.com>
2025-12-27 09:58:16 +0300

    [PW-6.7/cont] add-to-arr-min-max
    Solve 1st task. Work with "push(), docFragment(), forEach(), append()".
    
    QA-SB: B-5 / JS

--- 5-javascript/6-loops-and-arrays/6-7-pw-2-add-to-arr-min-max/index.js
+++ 5-javascript/6-loops-and-arrays/6-7-pw-2-add-to-arr-min-max/index.js
@@ -3,4 +3,22 @@
 // Задание 1:
 // Есть массив products, в котором хранится список товаров. Необходимо добавить в этот массив названия новых товаров: "Монитор", "Принтер", "Флешка" — и вывести их внутри списка ul.
 
+const productsList = document.querySelector('.products');
 const products = ['Мышка', 'Клавиатура', 'Наушники'];
+
+products.push('Монитор', 'Принтер', 'Флешка');
+
+const documentFragment = document.createDocumentFragment();
+
+products.forEach((item) => {
+  const productsItem = document.createElement('li');
+  productsItem.classList.add('products-item');
+  productsItem.textContent = item;
+
+  documentFragment.append(productsItem);
+});
+
+productsList.append(documentFragment);
+
+// Задание 2:
+//
