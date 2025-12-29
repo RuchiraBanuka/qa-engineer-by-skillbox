d448675139ddcc53b4d34ab14087690b494900b8
Sergey Matylionak <79189126+matowdev@users.noreply.github.com>
2025-12-29 09:56:56 +0300

    [PW-6.7/compl] add-to-arr-min-max
    Solve 2nd task. Work with "join(), listeners, for(), Math.min/max()".
    
    Worth noting:
      - all this work (alternative solution).
    
    QA-SB: B-5 / JS

--- 5-javascript/6-loops-and-arrays/6-7-pw-2-add-to-arr-min-max/index.js
+++ 5-javascript/6-loops-and-arrays/6-7-pw-2-add-to-arr-min-max/index.js
@@ -21,4 +21,34 @@ products.forEach((item) => {
 productsList.append(documentFragment);
 
 // Задание 2:
-//
+// Есть массив чисел numbers, который содержит несколько элементов. Выведите все элементы массива на странице. Также добавьте возможность находить минимальное и максимальное числа в массиве и отображать их значения на странице при нажатии соответствующих кнопок.
+
+const allElements = document.querySelector('.all-elements');
+const minBtn = document.querySelector('.min');
+const maxBtn = document.querySelector('.max');
+const minStrNum = document.querySelector('.minNumber');
+const maxStrNum = document.querySelector('.maxNumber');
+
+const numbers = [12, 5, 8, 20, 3, 16];
+const numbersStr = numbers.join(', ');
+
+allElements.textContent = numbersStr;
+
+minBtn.addEventListener('click', () => {
+  //   let minNum = numbers[0];
+  //   for (let i = 1; i < numbers.length; i++) {
+  //     if (numbers[i] < minNum) {
+  //       minNum = numbers[i];
+  //     }
+  //   }
+  //   minStrNum.textContent = minNum;
+
+  // ?? альтернативное решение
+  const minNum = Math.min(...numbers);
+  minStrNum.textContent = String(minNum); // textContent и так приведёт к строке, но.. так более явно
+});
+
+maxBtn.addEventListener('click', () => {
+  const maxNum = Math.max(...numbers);
+  maxStrNum.textContent = String(maxNum);
+});
